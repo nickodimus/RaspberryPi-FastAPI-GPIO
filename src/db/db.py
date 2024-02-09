937437b76994d8d39705afca195bd8443347724d
Nickodimus <skyagnitti@gmail.com>
2024-02-09 09:43:18 -0500

    commented out mariadb engine for future use

--- src/db/db.py
+++ src/db/db.py
@@ -6,9 +6,10 @@ sqlite_file_name_gpio = "gpio.db"
 sqlite_url_gpio = f"sqlite:///src/db/{sqlite_file_name_gpio}"
 sqlite_engine_gpio = create_engine(sqlite_url_gpio, echo=True, connect_args=connect_args)
 
+'''
 mariadb_url = "mariadb+mariadbconnector://sky:Fallon69@10.10.0.10:3306/tremelor"
 mariadb_engine = create_engine(mariadb_url, echo=True, connect_args=connect_args)
-
+'''
 def get_session():
     with Session(sqlite_engine_gpio) as session:
         yield session
