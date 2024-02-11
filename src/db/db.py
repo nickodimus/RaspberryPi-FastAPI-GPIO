171c4220d1a4089f92742e8bacd407334840d3a1
Nickodimus <skyagnitti@gmail.com>
2024-02-11 13:03:58 -0500

    updated src/db.oy to remove mariadb engine info since I'm not using it yet

--- src/db/db.py
+++ src/db/db.py
@@ -6,15 +6,10 @@ sqlite_file_name_gpio = "gpio.db"
 sqlite_url_gpio = f"sqlite:///src/db/{sqlite_file_name_gpio}"
 sqlite_engine_gpio = create_engine(sqlite_url_gpio, echo=True, connect_args=connect_args)
 
-'''
-mariadb_url = "mariadb+mariadbconnector://sky:Fallon69@10.10.0.10:3306/tremelor"
-mariadb_engine = create_engine(mariadb_url, echo=True, connect_args=connect_args)
-'''
+
 def get_session():
+    """
+     Get a session using the sqlite engine and yield the session object.
+     """
     with Session(sqlite_engine_gpio) as session:
         yield session
-
-'''
-def create_db_and_tables():
-    SQLModel.metadata.create_all(sqlite_engine_gpio)
-'''
